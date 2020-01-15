85b1cbb69ebf87ec911265bc5d402260f60d7934
Mark Mariscal <markamariscal@gmail.com>
2020-01-14 18:10:47 -0800

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,7 @@
+/*
+ * Title: palindrome.cpp
+ * Abstract: Write a C++ program “palindrome.cpp” that reads a string of characters from user and determines if the string is a palindrome or not.  For the program, you should store each character of the string in an array (use dynamic memory to create array(s)) and follow the algorithm described in the lecture.
+ * Author: Mark Mariscal
+ * ID: 7198
+ * Date: 01/14/2020
+ */
